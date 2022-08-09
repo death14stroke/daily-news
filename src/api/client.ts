7e51c9ea80825b11ef7b05516c6a383482147942
death14stroke <38498710+death14stroke@users.noreply.github.com>
2022-08-10 00:33:26 +0530

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -5,9 +5,4 @@ const instance = axios.create({
 	baseURL: Config.API_URL
 });
 
-instance.interceptors.request.use(async config => {
-	//await new Promise(resolve => setTimeout(resolve, 50000000));
-	return config;
-});
-
 export default instance;
